2d1f0b70026e07c33a85d8aa38d30c9d7a60799d
САША <alexander228sivkov@yandex.ru>
2024-05-02 11:38:34 +0500

    Исправил баг с подключением к БД

--- data/db_session.py
+++ data/db_session.py
@@ -19,7 +19,7 @@ def global_init(db_file):
 
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
 
-    engine = sa.create_engine(conn_str, echo=False, pool_size=20, max_overflow=0)
+    engine = sa.create_engine(conn_str, echo=False, pool_size=10, max_overflow=20)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
